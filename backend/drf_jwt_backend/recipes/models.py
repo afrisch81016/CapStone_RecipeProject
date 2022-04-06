be0024fe14f76caad15bcf436ae7deaca7a126cb
Adam Frisch <afrisch88@outlook.com>
2022-04-06 16:07:38 -0500

    resent migrations after making some adjustments

--- backend/drf_jwt_backend/recipes/models.py
+++ backend/drf_jwt_backend/recipes/models.py
@@ -4,10 +4,8 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Recipes(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    recipetitle = models.CharField(max_length=255)
-    recipeTimeTier = models.IntegerField()
-    ingredients = models.CharField(max_length=255)
+    ingredient = models.ForeignKey(User, on_delete=models.CASCADE), models.CharField(max_length=255)
+    favoritedRecipe = models.CharField(max_length=255)
     costOfIngredients = models.DecimalField(max_digits=6, decimal_places=2)
     bestByDate = models.DateField()
     expiredIngredients = models.IntegerField()
