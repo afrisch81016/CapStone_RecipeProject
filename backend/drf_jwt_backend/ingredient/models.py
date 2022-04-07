9de4e8e8bd73a7f526e59ffa3b6e4e74848a0b09
Adam Frisch <afrisch88@outlook.com>
2022-04-07 16:38:34 -0500

    migrations issues, fixed with instructors help

--- backend/drf_jwt_backend/ingredient/models.py
+++ backend/drf_jwt_backend/ingredient/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Ingredient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     best_by_date = models.DateField()
-    costOfIngredient = models.DecimalField(max_digits = 6, decimal_places=2,)
+    costOfIngredient = models.DecimalField(max_digits=6, decimal_places=2)
--- backend/drf_jwt_backend/ingredient/serializers.py
+++ backend/drf_jwt_backend/ingredient/serializers.py
@@ -6,3 +6,5 @@ class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ingredient
         fields = ['id','user','name','best_by_date','costOfIngredient']
+
+        user_id = serializers.IntegerField(write_only = True)
