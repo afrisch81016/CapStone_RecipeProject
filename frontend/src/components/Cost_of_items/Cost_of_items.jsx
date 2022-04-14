95af84d43281a43c610760d3c817791eccc25ce9
Adam Frisch <afrisch88@outlook.com>
2022-04-14 16:11:51 -0500

    had to move on from cost comparison due to backend data issues

--- frontend/src/components/Cost_of_items/Cost_of_items.jsx
+++ frontend/src/components/Cost_of_items/Cost_of_items.jsx
@@ -1,10 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import useAuth from "../../hooks/useAuth";
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import useAuth from "../../hooks/useAuth";
 
-const CostOfIngredientsInPantry = (props) =>{
+// const CostOfIngredientsInPantry = (props) =>{
     
-    function generateCostOfPantry(){
+//     // useEffect(()=> {
+//     //     generateCostOfPantry();
+//     // },[]);
 
-    }
-}
+//     function generateCostOfPantry(){
+
+//         let filteredIngredients = props.ingredients.filter(ingredients => props.ingredients);
+
+//         console.log(filteredIngredients);
+
+//         let costoffood = filteredIngredients.map(ingredients => {
+//             return ingredients.costOfIngredient;
+//         });
+//         console.log(costoffood);
+        
+//         let distinctIngredients = [...new Set(costoffood)]
+
+//         console.log(distinctIngredients)
+
+//         let IngredientsArrays = distinctIngredients.map(costoffood =>{
+//             let allCostOfItems = filteredIngredients.filter(ingredients => ingredients.costOfIngredient === ingredients);
+//             let totalCost = allCostOfItems.reduce(function(sum,current){
+//                 return sum + current.totalCost;
+//             },0)
+//             console.log(totalCost)
+
+//             return [IngredientsArrays]
+//         });
+         
+//     }
+//     return (
+//         <div>
+//             <table>
+//                 <td>
+//                     <tr>{generateCostOfPantry()}</tr>
+//                 </td>
+//             </table>
+//         </div>
+//     )
+
+// }
+
+
+// export default CostOfIngredientsInPantry
